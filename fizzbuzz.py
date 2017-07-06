dfeaf90d6bd6f83818bfcea1fe628c10c0321ecf
yuuma <reon.maple10@gmail.com>
2017-07-07 01:43:23 +0900

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,12 @@
-print('hello')
+print('数値を入力')
+num = input('>>>  ')
+for i in range(1,int(num)+1):
+	if ((i % 3 == 0) and (i % 5 == 0)) :
+		print('fizzbuzz')
+	elif (i % 3 == 0):
+		print('fizz')
+	elif (i % 5 == 0):
+		print('buzz')
+	else:
+		print(i)
+		
