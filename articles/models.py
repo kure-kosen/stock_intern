7935c5557db301130f895be8a03eedf691e555ff
chanyou0311 <yuukun0311@gmail.com>
2017-07-08 11:13:42 +0900

    Categoryモデル、Articleモデルの作成

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    now = datetime.datetime.now
+    category_name = models.CharField(max_length=128)
+    created = models.DateTimeField(default=now)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Article(models.Model):
+    now = datetime.datetime.now
+    text = models.TextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, db_column='category_id')
+    created = models.DateTimeField(default=now)
+    updated = models.DateTimeField(auto_now=True)
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from articles.models import Article, Category
+from accounts.models import User
 
-# Create your tests here.
+
+class ArticleModelTests(TestCase):
+    user_id = 'chanyou0311'
+    user_name = 'ちゃんゆー'
+    password = b'hogehoge'
+    category_name = '長期インターンシップ'
+    text = "親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。" \
+           "なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。"
+
+    def create_user(self):
+        user = User(user_id=self.user_id, user_name=self.user_name, password=self.password)
+        user.save()
+
+    def create_category(self):
+        category = Category(category_name=self.category_name)
+        category.save()
+
+    def test_category_model_ok(self):
+        category = Category(category_name=self.category_name)
+        category.save()
+
+        category_instance = Category.objects.get(id=1)
+        self.assertEqual(self.category_name, category_instance.category_name)
+
+    def test_article_model_ok(self):
+        self.create_user()
+        self.create_category()
+        user_instance = User.objects.get(user_id=self.user_id)
+        category_instance = Category.objects.get(id=1)
+        article = Article(text=self.text, user_id=user_instance, category_id=category_instance)
+        article.save()
+
+        article_instance = Article.objects.get(id=1)
+        self.assertEqual(self.text, article_instance.text)
