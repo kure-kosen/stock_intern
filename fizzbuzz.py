3bd027f9a162c8c25b500722cf3db85ec9d3aca3
yuuma <reon.maple10@gmail.com>
2017-07-08 21:35:28 +0900

    指摘修正

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 print('数値を入力')
 num = input('>>>  ')
-for i in range(1,int(num)+1):
-	if ((i % 3 == 0) and (i % 5 == 0)) :
+for i in range(1, int(num)+1):
+	if ((i % 3 == 0) and (i % 5 == 0)):
 		print('fizzbuzz')
 	elif (i % 3 == 0):
 		print('fizz')
