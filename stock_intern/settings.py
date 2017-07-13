64546276c258ed30c585cd79b2acace5709f9ed7
seed <miwadaiti0828@gmail.com>
2017-07-13 23:29:01 +0900

    staticフォルダの設定

--- stock_intern/settings.py
+++ stock_intern/settings.py
@@ -120,3 +120,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static files directory
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
