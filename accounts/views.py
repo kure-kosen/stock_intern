7ce1e1d894518b96308b737b93c556ab3a43524f
chanyou0311 <yuukun0311@gmail.com>
2017-07-29 22:47:46 +0900

    Test accounts form

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .forms import MyForm
 
 def form_test(request):
-    form = MyForm()
+    if request.method == "POST":
+        form = MyForm(data=request.POST)
+        if form.is_valid():
+            print(form.data.get('text'))
+    else:
+        form = MyForm()
     return render(request, 'accounts/form.html', {
         'form': form,
     })
