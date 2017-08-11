d5a958b25d6b6d4e1caa15665a4f14f9fbb76e3b
mba2014early <mba2014early@example.com>
2017-08-11 17:39:30 +0900

    パスワードを非表示にした

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from .models import User
 
-class UserForm(forms.Form):
+class UserForm(forms.ModelForm):
     user_id = forms.CharField()
     user_name = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("user_id", "user_name", "password")
