51d0a338a522542a5dc1000d615beb00b7e81503
mba2014early <mba2014early@example.com>
2017-08-12 23:50:39 +0900

    update requirements.txt

--- stock_intern/settings.py
+++ stock_intern/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.markup',
+    'markdown_deux',
 ]
 
 MIDDLEWARE = [
