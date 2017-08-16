fbfb58c183e7d69c35bf2dfd54db24f26748b1dd
yuuma <reon.maple10@gmail.com>
2017-08-16 19:18:41 +0900

    add accounts index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from django.core.urlresolvers import reverse_lazy
 from .forms import UserForm
 from django import forms
 from .models import User
+
+
+class IndexView(ListView):
+    model = User
+    template_name = 'accounts/index.html'
+    context_object_name = 'users'
 
 
 class UserCreate(CreateView):
